12d67994906e947ba2de5ec40456a271d80bef74
kacperob <kacper.obrebski00@gmail.com>
2024-05-22 10:28:49 +0200

    And_ zmiany w filtrowaniu

--- src/app/main.py
+++ src/app/main.py
@@ -17,6 +17,8 @@ def get_cars(session: Session = Depends(get_session)):
 def get_cars_by_brand(brand: str,
                       price_min: int = Query(None, alias="priceMin"),
                       price_max: int = Query(None, alias="priceMax"),
+                      mileage_min: int = Query(alias="mileageMin"),
+                      mileage_max: int = Query(alias="mileageMax"),
                       session: Session = Depends(get_session)):
     query_cars = session.query(CarsTable).filter_by(brand=brand)
     if price_min is not None:
@@ -24,12 +26,13 @@ def get_cars_by_brand(brand: str,
     if price_max is not None:
         query_cars = query_cars.filter(CarsTable.price <= price_max)
 
+    query_cars = query_cars.filter(CarsTable.mileage <= mileage_max,
+                                   CarsTable.mileage >= mileage_min)
+
     filtered_cars = query_cars.all()
 
     if not filtered_cars:
-        message = {"warining": f"No cars found"}
+        message = {"warning": "No cars found"}
         return JSONResponse(status_code=status.HTTP_200_OK, content=message)
 
-
     return {"debug": price_min, "message": filtered_cars}
-
